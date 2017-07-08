df9bf1ff0f9575c7a95500a23f29b75855fa489b
Yuki Jiru <Yuki Jiru>
2017-07-08 12:47:28 -0400

    reducing file size

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,17 @@ var config = {
     }]
 },
 plugins: [
-        extractSass
+        extractSass,
+        new webpack.optimize.UglifyJsPlugin({
+  compressor: {
+    warnings: false
+  }
+}),
+new webpack.DefinePlugin({
+  'process.env': {
+    'NODE_ENV': JSON.stringify('production')
+  }
+})
     ]
 };
 
